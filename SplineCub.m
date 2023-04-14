cbee79674872aa8cc15f93eeebd9edc9490bc456
anruki <79452048+anruki@users.noreply.github.com>
2023-04-14 21:56:01 +0200

    Update SplineCub.m

--- SplineCub.m
+++ SplineCub.m
@@ -22,7 +22,6 @@ function [Yint] = SplineCub(x,y,xint)
     M = zeros(n,n);
     % Calculo de la matriz tridiagonal M y del vector columna v de términos
     % independientes
-    disp(x)
     for i = 1:n-1
         h(i) = x(i+1) - x(i);
         b(i) = 1/h(i) * (y(i+1) - y(i));
@@ -31,20 +30,17 @@ function [Yint] = SplineCub(x,y,xint)
         u(j+1) = 2*(h(j) + h(j+1));
         v(j+1) = 6*(b(j+1) - b(j));
     end
-    disp(u)
     M(1:n+1:end) = u;   % Diagonal principal
     M(2:n+1:end) = h;   % Diagonal superior
     M(1,2) = 0;
     M(n+1:n+1:end) = h; % Diagonal inferior
     M(n,n-1) = 0;
-    disp(M)
     % Resolución del sistema
     a = Tridiagonal(M, v);
     
     % Se obtiene el valor interpolado de y en Xint
-    Yint = 0;
-    for i = 1:n-1
-        Yint = Yint + a(i)/(6*h(i))*(x(i+1)-xint)^3 + a(i+1)/(6*h(i))*(xint - x(i))^3 + (y(i)/h(i)-(a(i)*h(i))/6)*(x(i+1) - xint) + (y(i+1)/h(i) - (a(i+1)*h(i))/6)*(xint - x(i));
-    end
+    v = x-xint;
+    [~, i] = min(abs(v));
+    Yint = a(i)/(6*h(i))*(x(i+1)-xint)^3 + a(i+1)/(6*h(i))*(xint - x(i))^3 + (y(i)/h(i)-(a(i)*h(i))/6)*(x(i+1) - xint) + (y(i+1)/h(i) - (a(i+1)*h(i))/6)*(xint - x(i));
 
 end
