823bedf4e0b0418bb244ec1f66fc180f7253f5f2
Alf-caput <alfre.roble.aba@gmail.com>
2023-04-15 22:08:48 +0200

    minor fix

--- SplineCub.m
+++ SplineCub.m
@@ -7,37 +7,28 @@ function [Yint] = SplineCub(x,y,xint)
 %   xint = coordenada x del punto interpolado
 % OUTPUT:
 %   Yint = valor y del punto interpolado
-    % Calculo de coeficientes
+    % Cálculo de coeficientes
     n = length(x);
     % Inicialización de variables
-    b = zeros(n-1,1);
-    h = zeros(n-1,1);
-    u = zeros(n,1);
-    u(1) = 1;
-    u(n) = 1;
-    v = zeros(n,1);
-    v(1) = 0;
-    v(n) = 0;
-    M = zeros(n,n);
-    % Calculo de la matriz tridiagonal M y del vector columna v de términos
-    % independientes
-    for i = 1:n-1                           % Calculo de los vectores h y b
+    b = zeros(n-1, 1);
+    h = zeros(n-1, 1);
+    % Calculo de los vectores h y b
+    for i = 1:n-1
         h(i) = x(i+1) - x(i);
         b(i) = 1/h(i) * (y(i+1) - y(i));
     end
-    for j = 1:n-2                           % Calculo de los vectores u y v                             
-        u(j+1) = 2*(h(j) + h(j+1));
-        v(j+1) = 6*(b(j+1) - b(j));
+    % Calculo de los vectores u y v
+    u = ones(n, 1);
+    v = zeros(n, 1);
+    for i = 1:n-2
+        u(i+1) = 2*(h(i) + h(i+1));
+        v(i+1) = 6*(b(i+1) - b(i));
     end
-    M(1:n+1:end) = u;   % Diagonal principal
-    M(2:n+1:end) = h;   % Diagonal superior
-    M(1,2) = 0;
-    M(n+1:n+1:end) = h; % Diagonal inferior
-    M(n,n-1) = 0;
-
+    M = diag(u) + diag(h, 1) + diag(h, -1); 
+    M(1, 2) = 0;
+    M(n, n-1) = 0;
     % Resolución del sistema
     a = Tridiagonal(M, v);
-    
     % Se obtiene el valor interpolado de y en Xint
     v = x-xint;
     [~, i] = min(abs(v));
