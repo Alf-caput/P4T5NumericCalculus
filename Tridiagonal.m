a79aecbb5be5b92859e61f46e06e3d73c5ae302d
anruki <79452048+anruki@users.noreply.github.com>
2023-04-16 14:09:08 +0200

    Update Tridiagonal.m

--- Tridiagonal.m
+++ Tridiagonal.m
@@ -1,41 +1,60 @@
-function x = Tridiagonal(A,b)
+function x = Tridiagonal(A,B)
 % Función que busca la solución de un sistema de ecuaciones por el método
 % de Thomas.
-
 % INPUTS:
 % A = matriz de coeficientes del sistema de ecuaciones que debe de ser
-% tridiagonal
-% b = vector fila de términos independientes (es la columna traspuesta)
+% cuadrada y tridiagonal.
+% B = vector columna de términos independientes (es la columna traspuesta)
 
 % OUTPUTS:
 % x = vector columna solución del sistema de ecuaciones
+    % Primero verificamos que la matriz sea cuadrada y tridiagonal:
+    [Z, F] = size(A);
+    if Z~=F 
+        disp('ERROR.La matriz no es cuadrada.');
+        return
+    end
 
-    % Obtener los tamaños de la matriz y del vector
-    n = length(b);
-    
-    % Inicializar los vectores necesarios para el algoritmo
-    c = zeros(n-1,1);
-    d = zeros(n,1);
-    x = zeros(n,1);
-    
-    % Algoritmo de Thomas para resolver el sistema tridiagonal
-    c(1) = A(1,2)/A(1,1);
-    d(1) = b(1)/A(1,1);
-    
-    for i = 2:n-1
-        c(i) = A(i,i+1)/(A(i,i) - A(i,i-1)*c(i-1));
+    for i = 1:F
+        for j = 1:F
+            if abs(i-j) > 1 && A(i,j) ~= 0
+                disp('ERROR.La matriz no es tridiagonal')
+                return 
+            end
+        end
     end
-    
-    d(2) = (b(2) - A(2,1)*d(1))/(A(2,2) - A(2,1)*c(1));
-    
-    for i = 3:n
-        d(i) = (b(i) - A(i,i-1)*d(i-1))/(A(i,i) - A(i,i-1)*c(i-1));
+
+    % Vector que contiene la diagonal de A
+    d = diag(A);
+    % Diagonal superior
+    a = diag(A,1);
+    % Diagonal inferior con el primer elemento 0 y el último A(F,F)
+    b = zeros(F,1);
+    b(2:F) = diag(A,-1);
+    d(F) = A(F,F);
+
+    % A prima 
+    ap = zeros(F-1,1);
+    ap(1) = a(1)/d(1);
+    % B prima
+    Bp = zeros(F,1);
+    Bp(1) = B(1)/d(1);
+    % B prima prima
+    Bpp = zeros(F,1);
+
+    for i=2:F-1
+        ap(i) = a(i) / (d(i) - b(i)*ap(i-1));
+        Bpp(i) = (B(i)-b(i)*Bp(i-1)) / (d(i)-b(i)*ap(i-1));
     end
+    Bpp(F) = (B(F)-b(F)*Bp(F-1)) / (d(F) - b(F)*ap(F-1));
     
-    x(n) = d(n);
-    
-    for i = n-1:-1:1
-        x(i) = d(i) - c(i)*x(i+1);
+    x = zeros(F,1);
+    x(F) = Bpp(F);
+
+    % Hacemos sustitucion hacia atras
+    for i=1:F-1
+        x(F-i) = Bpp(F-i) - ap(F-i)*x(F-i+1);
     end
-    
-    end
+
+
+end
