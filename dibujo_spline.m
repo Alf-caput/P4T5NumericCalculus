ebbc414a76b4f70071fe05daac75385f221dd141
Alf-caput <alfre.roble.aba@gmail.com>
2023-04-16 20:20:40 +0200

    red

--- dibujo_spline.m
+++ dibujo_spline.m
@@ -6,7 +6,7 @@ for i = 1: n
 end
 p1 = plot(xx, yy, 'b-');
 hold on
-p2 = plot(T, h, '.', MarkerSize=10);
+p2 = plot(T, h, 'r.', MarkerSize=10);
 % Documentación y ajustes del gráfico
 title("Spline con polinomios de Lagrange");
 xlabel('$\mathit{T}$ $\mathit{x}$ $10^{3}$ $\mathit{(K)}$','Interpreter','latex','FontSize',8);
